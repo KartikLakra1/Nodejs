d921e200aaab417089ee7cd964cf1508765d63c5
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-11-10 14:00:45 +0530

    http server

---
+++ FirstNodeServer/app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req,res)=>{
+    console.log('Request made to the server');
+    fs.appendFile('log.txt', `Request made at: ${new Date().toString()} with IP address ${req.socket.remoteAddress}\n`, (err)=>{
+        if(err){
+            console.error('Error writing to log file',err);
+        }else console.log('Log updated');
+    })
+})
+
+
+const PORT = 3000;
+
+server.listen(PORT , ()=>{
+    console.log(`Server is listening on http://129.0.0.1:${PORT}`)
+})
