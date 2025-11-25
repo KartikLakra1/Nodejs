ca5d6b9a315c7b681e0f1f09701ca0f289bb3f27
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-11-25 23:20:35 +0530

    filehandling and http server creation

--- fileHandling/first.js
+++ fileHandling/first.js
@@ -1,9 +1,35 @@
-console.log('File handling in JavaScript ')
+const http = require("http");
+const fs = require("fs");
+console.log("File handling in JavaScript ");
 
-const fs = require('fs');
+const server = http.createServer((req, res) => {
+  fs.appendFileSync(
+    "./output.txt",
+    `Request made at: ${Date.now()} : ${req.url} with IP address ${
+      res.socket.remoteAddress
+    }\n`,
+    (err) => {
+      if (err) {
+        console.error("Erro writing to log file", err);
+      } else console.log("Log updated");
+    }
+  );
+
+  if (req.url == "/") {
+    res.write("<h1>Welcome to the Home Page</h1>");
+    res.end();
+  } else {
+    res.write("<h1>404 Page Not Found</h1>");
+    res.end();
+  }
+});
+
+server.listen(8000, () => {
+  console.log(`server is listening on http://localhost:8000`);
+});
 
 // writing in a file
-fs.appendFileSync('output.txt', '\nWriting in the output file kartik' , (err) => {
-    if(err) console.log(err);
-    else console.log('File written successfully')
-})
+// fs.appendFileSync('output.txt', '\nWriting in the output file kartik' , (err) => {
+//     if(err) console.log(err);
+//     else console.log('File written successfully')
+// })
